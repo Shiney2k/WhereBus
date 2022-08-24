d6671aba5fb36667ccfb443273bcaf8c23735bed
Dinu5346 <dinusha6326@outlook.com>
2022-08-24 20:25:58 +0530

    Update bus_item_cards.dart

--- lib/widgets/bus_item_cards.dart
+++ lib/widgets/bus_item_cards.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:wherebus/tools/get_bus_json_model.dart';
 import 'package:wherebus/tools/get_user_json_model.dart';
 import 'package:wherebus/widgets/big_text.dart';
 import 'package:wherebus/widgets/small_text.dart';
+
+import '../auth/secrets.dart';
 
 class BusItemCards extends StatefulWidget {
   const BusItemCards({Key? key, required this.data}) : super(key: key);
@@ -17,6 +23,62 @@ class _BusItemCardsState extends State<BusItemCards> {
   var _currPageValue = 0.0;
   final double _scaleFactor = 0.8;
   final double _height = 300;
+
+  var url = Uri.parse(MONGODB_URL_BASE + 'action/find');
+
+  Future<GetBusJsonModel> getBus(String ownerEmail) async {
+    HttpClient httpClient = HttpClient();
+    HttpClientRequest request = await httpClient.postUrl(url);
+    request.headers.set('Content-Type', 'application/json');
+    request.headers.set('api-key', MONGODB_API_KEY);
+    request.add(utf8.encode(json.encode({
+      'dataSource': 'Cluster0',
+      'database': 'WhereBus',
+      'collection': 'buses',
+      'filter': {"owner": ownerEmail}
+    })));
+    HttpClientResponse response = await request.close();
+    String reply = await response.transform(utf8.decoder).join();
+    // print(reply);
+    // var jsonReply = json.decode(reply);
+    httpClient.close();
+
+    if (response.statusCode == 200) {
+      return getBusJsonModelFromJson(reply);
+    } else {
+      showDialog<void>(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error ${response.statusCode}'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: const <Widget>[
+                  Text('Could not find your buses'),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text('Please try again later'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Ok'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+      return getBusJsonModelFromJson(json.encode(<String, dynamic>{
+        'documents': [{'_id': 'Error'}]
+      }));
+    }
+  }
 
   @override
   void initState() {
@@ -49,7 +111,9 @@ class _BusItemCardsState extends State<BusItemCards> {
                 return _buildPageItem(position);
               }),
         ),
-        const SizedBox(height: 14,),
+        const SizedBox(
+          height: 14,
+        ),
         DotsIndicator(
           dotsCount: 5,
           position: _currPageValue,
@@ -100,56 +164,86 @@ class _BusItemCardsState extends State<BusItemCards> {
             child: Padding(
               padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 24),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    BigText(text: 'Bus reg. no.:', color: Colors.white,),
-                    SmallText(text: 'XXX-0000', color: Colors.white,)
-                  ],
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    BigText(text: 'No. of seats:', color: Colors.white,),
-                    SmallText(text: '35', color: Colors.white,)
-                  ],
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    BigText(text: 'Driver contact:', color: Colors.white,),
-                    SmallText(text: '000000000', color: Colors.white,)
-                  ],
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    BigText(text: 'Bus type:', color: Colors.white,),
-                    SmallText(text: 'Expressway', color: Colors.white,)
-                  ],
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    BigText(text: 'Book seats:', color: Colors.white,),
-                    SmallText(text: 'Available', color: Colors.white,)
-                  ],
-                ),
-              ]),
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        BigText(
+                          text: 'Bus reg. no.:',
+                          color: Colors.white,
+                        ),
+                        SmallText(
+                          text: 'XXX-0000',
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        BigText(
+                          text: 'No. of seats:',
+                          color: Colors.white,
+                        ),
+                        SmallText(
+                          text: '35',
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        BigText(
+                          text: 'Driver contact:',
+                          color: Colors.white,
+                        ),
+                        SmallText(
+                          text: '000000000',
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        BigText(
+                          text: 'Bus type:',
+                          color: Colors.white,
+                        ),
+                        SmallText(
+                          text: 'Expressway',
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        BigText(
+                          text: 'Book seats:',
+                          color: Colors.white,
+                        ),
+                        SmallText(
+                          text: 'Available',
+                          color: Colors.white,
+                        )
+                      ],
+                    ),
+                  ]),
             )));
   }
 }
